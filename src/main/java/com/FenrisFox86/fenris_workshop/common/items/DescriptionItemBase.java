17829fa65e4f6f9777ee0059a10d07f812ad01c6
FenrisFox <69804647+FenrisFox@users.noreply.github.com>
2021-07-30 09:50:55 +0200

    changed silver textures

--- src/main/java/com/FenrisFox86/fenris_workshop/common/items/DescriptionItemBase.java
+++ src/main/java/com/FenrisFox86/fenris_workshop/common/items/DescriptionItemBase.java
@@ -1,7 +1,9 @@
 package com.FenrisFox86.fenris_workshop.common.items;
 
 import com.FenrisFox86.fenris_workshop.FenrisWorkshop;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.client.util.InputMappings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
@@ -9,6 +11,7 @@ import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.lwjgl.glfw.GLFW;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,16 +19,29 @@ import java.util.List;
 public class DescriptionItemBase extends Item {
 
     private final TranslationTextComponent[] translationComponents;
+    private final String[] text_info;
 
     public DescriptionItemBase(TranslationTextComponent... translation_text_Components) {
         super(new Properties().tab(FenrisWorkshop.MOD_TAB));
         this.translationComponents = translation_text_Components;
+        this.text_info = null;
+    }
+
+    public DescriptionItemBase(String... text_info) {
+        super(new Properties().tab(FenrisWorkshop.MOD_TAB));
+        this.text_info = text_info;
+        this.translationComponents = null;
     }
 
     @Override
     @OnlyIn(Dist.CLIENT)
     public void appendHoverText(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         super.appendHoverText(stack, worldIn, tooltip, flagIn);
-        tooltip.addAll(Arrays.asList(translationComponents));
+        if(InputMappings.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_SHIFT)) {
+            if (translationComponents != null) {tooltip.addAll(Arrays.asList(translationComponents));}
+            if (text_info != null) {for (String desc: text_info) { tooltip.add(new TranslationTextComponent(desc)); }}
+        } else {
+            tooltip.add(Tooltips.HOLD_SHIFT);
+        }
     }
 }
